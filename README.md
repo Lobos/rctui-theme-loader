81ae2ef0e67f9c10a3e073b7e23447cab9d68647
lobos <lobos841@gmail.com>
2017-04-22 03:43:08 +0800

    loader file

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-rctui example loader for webpack
+rctui组件变量替换工具，编译时替换默认变量
+
+```
+$ npm install rctui-theme-loader --save-dev
+```
+
+webpack.config 配置
+
+
+```
+{
+  test: /\.scss$/,
+  use: [
+    { loader: 'style-loader' },
+
+    { loader: 'css-loader' },
+
+    { loader: 'sass-loader' },
+
+    {
+      loader: 'rctui-theme-loader',
+      options: {
+        // 少量的变量可以通过string传入
+        variables: '$brand-primary: blue;',
+        // 大量变量可以传入文件
+        files: ['src/_variables.scss']
+      }
+    }
+  ]
+}
+```
+
+变量名称参考[项目下的bootstrap变量文件](https://github.com/Lobos/react-ui/blob/master/src/styles/themes/_bootstrap.scssa)
+
--- index.js
+++ index.js
@@ -1,9 +1,32 @@
-var loaderUtils = require('loader-utils');
+const loaderUtils = require('loader-utils')
+const fs = require('fs')
+
+const cache = {}
+function getContent (f) {
+  if (cache[f]) return cache[f]
+
+  const content = fs.readFileSync(f).toString()
+  cache[f] = content
+  return content
+}
 
 module.exports = function (content) {
-  var query = loaderUtils.parseQuery(this.query);
-  if (query.theme) {
-    content = content.replace('themes/bootstrap', `themes/${query.theme}`);
+  const options = loaderUtils.getOptions(this)
+  if (options.theme) {
+    content = content.replace('themes/default', 'themes/' + options.theme)
   }
-  return content;
+
+  if (options.variables) {
+    content = options.variables + content
+  }
+
+  if (options.files) {
+    const files = typeof options.files === 'string' ? [options.files] : options.files
+    files.forEach(function (f) {
+      const str = getContent(f)
+      content = str + content
+    })
+  }
+
+  return content
 }
